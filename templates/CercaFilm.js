b69bd62ff98e9bbf2a55878d1bac7073015461aa
Giorgio Nocerino <74252212+Giorgio-Nocerino@users.noreply.github.com>
2022-12-14 11:12:28 +0100

    Add files via upload

---
+++ templates/CercaFilm.js
@@ -0,0 +1,79 @@
+const movies = document.querySelector('.movies');
+const form = document.querySelector('#form');
+let zIndex = 0;
+let mov = [];
+const jsonData = new Request('movies.json');
+
+//recupero dati dal file json
+const getData = async()=>{
+    try{//risposta
+        let response = await fetch(jsonData);
+        let data = await response.json();
+        console.log(data)
+        data.Movies.forEach(m => mov.push(m));
+        //appende dal DOM
+        renderMovies(mov);
+        //seleziona la carta elemento on clicco
+        //moviesList()
+
+    }catch(err){
+        if(err) console.log(err)
+    }
+}
+
+//Run funzione
+getData()
+
+//Va in loop l'array e prende i valori dentro al DOM
+const renderMovies = (arr) =>{
+    movies.innerHTML = '';
+
+    for(let i=0; i< arr.length; i++){
+        movies.innerHTML += `
+         <div class="movie" data-top="${i}">
+                  <button class="box"><img src=${arr[i].img}" alt="" class="immagine"></button>
+                  <p class="title">${arr[i].name}</p>
+              </div>
+        `
+    }
+}
+
+//Seleziona il film
+
+const moviesList = () => {
+    let cards = document.querySelectorAll('.movie');
+
+    cards.forEach(c => {
+        //incrementa il top margin
+        c.style.top = c.dataset.top * 50;
+        //aggiunge il margine di destra
+        c.style.marginRight = c.dataset.top + 5;
+        //aggiunge l'evento click
+        c.onclick = ()=>{
+            zIndex++
+
+            c.style.zIndex = zIndex;
+
+            c.querySelector('.title').style.background = "yellow";
+        }
+    });
+}
+
+
+
+//Cerca il film
+
+form.onkeyup = e =>{
+    //fa l'evento
+    e.preventDefault();
+    //input value
+    let search = form.search?.value?.toLowerCase();
+    // vede l'array
+    let results = mov.filter(m => {
+        return m.name.toLowerCase().includes(search);
+    });
+
+    renderMovies(results);
+
+    moviesList();
+}
