537d03241e770af3a78bec7c2a10ca5d76d5f47e
Giorgio-Nocerino <giorgio.nocerino001@studenti.uniparthenope.it>
2022-12-17 23:00:09 +0100

    Carousel

---
+++ static/js/Carousel.js
@@ -0,0 +1,28 @@
+var n_blocks = 15;
+var current = 1;
+
+function successivo(){
+    current++;
+    if(current > n_blocks){
+        current = 1;
+    }
+
+    for(var i = n_blocks; i > 0; i--){
+        document.querySelector(".cast .actors .actor:nth-of-type(" + i + ")").style.display = "none";
+    }
+    document.querySelector(".cast .actors .actor:nth-of-type(" + current + ")").style.display = "block";
+}
+
+function precedente(){
+    current--;
+    if(current == 0){
+        current = n_blocks;
+    }
+
+    for(var i = n_blocks; i > 0; i--){
+        document.querySelector(".cast .actors .actor:nth-of-type(" + i + ")").style.display = "none";
+    }
+    document.querySelector(".cast .actors .actor:nth-of-type(" + current + ")").style.display = "block";
+
+    setInterval(successivo, 5000);
+}
