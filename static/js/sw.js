04b4ffb4c485f7b7b2cb03ada6500fce82aaf9c1
Giorgio-Nocerino <giorgio.nocerino001@studenti.uniparthenope.it>
2022-12-22 13:43:46 +0100

    Service Worker

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,40 +1,72 @@
 let cacheName = 'Tech_Web_Progetto';
 let filesToCache = [
-    'templates/HorrorMovieland.html',
-    'templates/LoginHML.html',
-    'templates/SignupHML.html',
-    'templates/Account.html',
-    'templates/Manual.html',
-    'templates/Movie1.html',
-    'templates/Prepage.html',
-    'static/stylesheet/Style.css',
-    'static/stylesheet/AccountStyle.css',
-    'static/stylesheet/LoginStyle.css',
-    'static/stylesheet/SignupStyle.css',
-    'static/stylesheet/ManualStyle.css',
-    'static/stylesheet/MovieStyle.css',
-    'static/js/Carousel.js',
-    'static/js/SearchMovies.js',
-    'static/js/main.js'
+    '/',
+    '/static/stylesheet/PrepageStyle.css',
+    '/static/images/skull.png'
 ];
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
     );
 });
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
+
+
+
+
+
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
         })
     );
+    return self.clients.claim();
 });
-/* Service worker activated */
-self.addEventListener('activate', (e) => {
-    console.log("Service worker is activated");
+
+
+
+
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request).catch(error => {
+                console.log('Fetch failed; returning offline page instead.', error);
+                //let url = e.request.url;
+                //let extension = url.split('.').pop();
+
+                /*if (extension === 'jpg' || extension === 'png') {
+
+                    const FALLBACK_IMAGE = `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="180" stroke-linejoin="round">
+                <path stroke="#DDD" stroke-width="25" d="M99,18 15,162H183z"/>
+                <path stroke-width="17" fill="#FFF" d="M99,18 15,162H183z" stroke="#eee"/>
+                <path d="M91,70a9,9 0 0,1 18,0l-5,50a4,4 0 0,1-8,0z" fill="#aaa"/>
+                <circle cy="138" r="9" cx="100" fill="#aaa"/>
+                </svg>`;
+
+                    return Promise.resolve(new Response(FALLBACK_IMAGE, {
+                        headers: {
+                            'Content-Type': 'image/svg+xml'
+                        }
+                    }));
+                }*/
+
+                return caches.match('offline.html');
+            });
+        })
+    );
 });
 
