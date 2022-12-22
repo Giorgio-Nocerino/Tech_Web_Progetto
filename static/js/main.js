7ef6d5e248cda1c9c9f8555293ef93956164b331
Giorgio-Nocerino <giorgio.nocerino001@studenti.uniparthenope.it>
2022-12-22 13:42:30 +0100

    Main

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,52 @@
-window.onload = () => {
-    'use strict';
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/static/js/sw.js').then(function (registration) {
-
-            // Service worker registered correctly.
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    },
-    function (err) {
-            // Troubles in registering the service worker. :(
-            console.log('ServiceWorker registration failed: ', err);
-    });
-    }
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/static/js/sw.js')
+        .then(function(registration) {
+            console.log('Service Worker Registered');
+            return registration;
+        })
+        .catch(function(err) {
+            console.error('Unable to register service worker.', err);
+        });
 }
+
+
+
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = 'block';
+    addBtn.addEventListener('click', (e) => {
+        addBtn.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+
+
+                console.log('User accepted the A2HS prompt');
+
+
+            } else {
+
+                console.log('User dismissed the A2HS prompt');
+
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+
+
+
+window.addEventListener('online', function(e) {
+    console.log("You are online");
+}, false);
