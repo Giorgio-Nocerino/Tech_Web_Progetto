7487f24bff9736eb33aaf488e5e7f997b509ab86
Giorgio-Nocerino <giorgio.nocerino001@studenti.uniparthenope.it>
2023-01-08 12:53:15 +0100

    Carousel

--- static/js/Carousel.js
+++ static/js/Carousel.js
@@ -1,37 +1,3 @@
-/*function ShowCast(codex_movie){
-    var cod = codex_movie;
-    var xhttp = new XMLHttpRequest;
-
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200) {
-            var response = JSON.parse(xhttp.responseText);
-
-            var cast_carousel = response.cast_carousel;
-            var actor = '';
-
-            for(var i=0; i<cast_carousel.length; i++){
-                if(cod==cast_carousel[i].codex){
-                    actor+=`
-                            <button class="precedente" onclick="precedente()"></button>
-                            <div class="actor">
-                               <div class="boxy"><img src="${cast_carousel[i].profile}" alt="" class="image"></div>
-                               <p class="name">${cast_carousel[i].actor}</p>
-                            </div>
-                            <button class="successivo" onclick="successivo()"></button>`
-                }
-            }
-            var res_cast = document.getElementById("actors");
-            res_cast.innerHTML = actor;
-
-        }
-    };
-
-
-
-    xhttp.open("GET", "/static/js/cast_carousel.json", true);
-    xhttp.send();
-}*/
-
 var n_blocks = 5;
 var current = 1;
 
